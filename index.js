b3d35f3ee6af7a7006547c7a0b7463d03fea7771
Nikhil Nepal <nepal.nikhil902@gmail.com>
2024-07-18 11:15:02 +0545

    some changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const offCanvas = document.getElementById('offCanvas')
+const offCanvasBase = document.getElementById('offCanvasBase')
 
 function offCanvasExtend(){
     offCanvas.style.width = "350px";
-    offCanvas.style.border = "5px solid var(--teal)"
-    console.log('extend');
+    offCanvas.style.border = "5px solid var(--teal)";
+    offCanvasBase.style.width = "100vw";
 }
 
 function offCanvasClose(){
     offCanvas.style.width = "0px";
-    offCanvas.style.border = "0px"
-    console.log('close');
+    offCanvas.style.border = "0px";
+    offCanvasBase.style.width = "0px";
 }
